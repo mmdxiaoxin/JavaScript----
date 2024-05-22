bd1b3c4724c18368f1b8d6494c344a1a8d98cdcc
小新 <782446723@qq.com>
2024-05-22 21:06:51 +0800

    fix: 手写promise.all

--- JavaScript/4.Promise相关/promiseAll.js
+++ JavaScript/4.Promise相关/promiseAll.js
@@ -1,10 +1,10 @@
 const promise1 = Promise.resolve(1);
-const promise2 = Promise.resolve(2);
+const promise2 = Promise.reject(2);
 const promise3 = Promise.resolve(3);
 
 Promise.all([promise1, promise2, promise3])
     .then((values) => {
-        console.log(values); // 输出: [1, 2, 3]
+        console.log('all:', values); // 输出: [1, 2, 3]
     })
     .catch((error) => {
         console.error(error);
@@ -12,32 +12,41 @@ Promise.all([promise1, promise2, promise3])
 
 /**
  * 自定义Promise.all
- * @param {Iterable} proms
+ * @param {Iterable} values
  */
-Promise.myAll = function (proms) {
-    let res, rej;
-    const p = new Promise((resolve, reject) => {});
-
-    let count = 0;
-    const result = [];
-    let i = 0;
-    let fulfilledCount = 0;
-    for (const prom of proms) {
-        const index = i;
-        i++;
-        count++;
-        Promise.resolve(prom).then((data) => {
-            // 1. data -> result
-            result[index] = data;
-            // 2.完成最终的promise
-            fulfilledCount++;
-            if (fulfilledCount === count) {
-                res(result);
+Promise.myAll = function (values) {
+    if (!Array.isArray(values)) {
+        const type = typeof values;
+        return new TypeError(
+            `TypeError: ${type} ${values} is not iterable`,
+        );
+    }
+    return new Promise((resolve, reject) => {
+        const resultArr = [];
+        let orderIndex = 0;
+        const processResultByKey = (value, index) => {
+            resultArr[index] = value;
+            if (++orderIndex === values.length) {
+                resolve(resultArr);
             }
-        }, rej);
-    }
-    if (count === 0) {
-        res(result);
-    }
-    return p;
+        };
+        for (let i = 0; i < values.length; i++) {
+            let value = values[i];
+            if (value && typeof value.then === 'function') {
+                value.then((value) => {
+                    processResultByKey(value, i);
+                }, reject);
+            } else {
+                processResultByKey(value, i);
+            }
+        }
+    });
 };
+
+Promise.myAll([promise1, promise2, promise3])
+    .then((values) => {
+        console.log('myAll:', values); // 输出: [1, 2, 3]
+    })
+    .catch((error) => {
+        console.error(error);
+    });
