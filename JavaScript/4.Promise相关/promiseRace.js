b01fc9a5cb036e1b193b477f55ee3600eb0feffe
小新 <782446723@qq.com>
2024-05-22 21:07:08 +0800

    fix: 手写promise.race

--- JavaScript/4.Promise相关/promiseRace.js
+++ JavaScript/4.Promise相关/promiseRace.js
@@ -1,5 +1,9 @@
-const promise1 = new Promise((resolve) => setTimeout(resolve, 100, 'one'));
-const promise2 = new Promise((resolve) => setTimeout(resolve, 200, 'two'));
+const promise1 = new Promise((resolve) =>
+    setTimeout(resolve, 100, 'one'),
+);
+const promise2 = new Promise((resolve) =>
+    setTimeout(resolve, 200, 'two'),
+);
 
 Promise.race([promise1, promise2])
     .then((value) => {
@@ -8,3 +12,15 @@ Promise.race([promise1, promise2])
     .catch((error) => {
         console.error(error);
     });
+
+/**
+ * 自定义race
+ * @param {Iterable} promises
+ */
+Promise.myRace = function (promises) {
+    return new Promise((resolve, reject) => {
+        for (const promise of promises) {
+            Promise.resolve(promise).then(resolve).catch(reject);
+        }
+    });
+};
