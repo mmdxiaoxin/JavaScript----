55952505767690ff744406dc8c14ac9dc98b50e9
小新 <782446723@qq.com>
2024-05-22 21:07:44 +0800

    feat: 手写promise

--- JavaScript/4.Promise相关/myPromise.js
+++ JavaScript/4.Promise相关/myPromise.js
@@ -1,21 +1,60 @@
-class MyPromise {
-    constructor(fn) {
-        this.state = 'pending';
+const PENDING = 'pending';
+const FULFILLED = 'fulfilled';
+const REJECTED = 'rejected';
 
-        this.successFn = [];
+class Promise {
+    constructor(executor) {
+        this.status = PENDING;
+        this.value = undefined;
+        this.reason = undefined;
+        this.onResolvedCallbacks = [];
+        this.onRejectedCallbacks = [];
 
-        this.failFn = [];
-
-        let resolve = (val) => {
-            if (this.state !== 'pending') return;
-
-            this.state = 'success';
+        let resolve = (value) => {
+            if (this.status === PENDING) {
+                this.status = FULFILLED;
+                this.value = value;
+                this.onResolvedCallbacks.forEach((func) =>
+                    func(),
+                );
+            }
         };
 
-        let reject = (err) => {};
+        let reject = (reason) => {
+            if (this.status === PENDING) {
+                this.status = REJECTED;
+                this.value = this.value;
+                this.onRejectedCallbacks.forEach((func) =>
+                    func(),
+                );
+            }
+        };
+
+        try {
+            executor(resolve, reject);
+        } catch (error) {
+            reject(error);
+        }
     }
 
-    then(resolveCallback, rejectCallback) {}
+    then(onFulfilled, onRejected) {
+        if (this.status === FULFILLED) {
+            onFulfilled(this.value);
+        }
+
+        if (this.status === REJECTED) {
+            onRejected(this.reason);
+        }
+
+        if (this.status === PENDING) {
+            this.onResolvedCallbacks.push(() =>
+                onFulfilled(this.value),
+            );
+            this.onRejectedCallbacks.push(() =>
+                onRejected(this.value),
+            );
+        }
+    }
 
     static all(promiseArr) {}
 
